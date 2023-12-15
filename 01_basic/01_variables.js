8ce7b339778ec24bff8eb7e102e39a38fb5b3d4f
soamtripathy <soumyaranjantripathy44@gmail.com>
2023-12-15 22:29:55 +0530

    data types

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -3,5 +3,15 @@ let accountEmail = "soumya@gmail.com"
 var accountPassword = "12345"
 accountCity = "Rampur"
 // accountId = 2 //not allowed
+accountEmail = "soam@soam.com"
+accountPassword = "212121"
+accountCity = "Narla"
+let accountState;
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 
 console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as a newer version
+// alert("Hello") //we are using nodeJS, not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+console.log("Soam Tripathy")
+
+let name = "Soam"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique 
+
+// object 
+console.log(typeof undefined);
