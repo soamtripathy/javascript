d5d60f13f39fce343bb7dbc1bf89f31c189d41d0
soamtripathy <soumyaranjantripathy44@gmail.com>
2023-12-18 23:31:31 +0530

    dataTypes completed

---
+++ 01_basic/dataTypes_Summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// JavaScript is Dynamic Programming Language
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIN = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+const bigNumber = 3493023422322323n
+
+// Reference Type/ Non primitive
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name : "Soumya",
+    age : "23",
+}
+
+const myFunction = function(){
+        console.log("hello world!");
+}
+console.log(typeof bigNumber);
