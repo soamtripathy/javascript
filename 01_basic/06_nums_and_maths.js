6c2736b9f1a265bc60ba20672dc9ffffec243fc9
soamtripathy <soumyaranjantripathy44@gmail.com>
2023-12-23 23:45:22 +0530

    math added

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+// const score = 47
+
+// console.log(score);
+// const balance = new Number(100)
+
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.0648
+
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreads = 100000000
+
+
+// console.log(hundreads.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++
+
+
