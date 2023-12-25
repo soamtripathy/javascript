18caf3ce45e63c5a74d2376c414a98d00da03b4f
soamtripathy <soumyaranjantripathy44@gmail.com>
2023-12-25 16:59:33 +0530

    arraynode .\02_basic\02_array.js

---
+++ 02_basic/02_array.js
@@ -0,0 +1,35 @@
+// const marvel_heros = ["thor", "Ironman", "spiderman"]
+
+// const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+
+// console.log(marvel_heros[3][1]);
+
+// marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros)
+
+// console.log(allHeros);
+
+// const allNewHeros = [...marvel_heros, ...dc_heros]
+
+// console.log(allNewHeros);
+
+// const anotherArray = [1, 2, 3, [4, 5, 6], 7, [7,[3, 8]]]
+
+// const real_anotherArray = anotherArray.flat(Infinity)
+
+// console.log(real_anotherArray);
+
+// console.log(real_anotherArray.sort());
+
+// console.log(Array.isArray("Soumya"));
+// console.log(Array.from("Soumya"));
+// console.log(Array.from({name: "Soumya"}));  //intresting 
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
+
