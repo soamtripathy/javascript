104bb402a3aa7bce9caed98225572f227419f55e
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-01-25 14:32:54 +0530

    conditional

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+// if
+
+// const isUserLoggedIn = true;
+// const temparature = 41;
+
+// if (temparature === 50) {
+//   console.log("less than 50");
+// } else {
+//   console.log("temperature is greater than 50");
+// }
+
+// <, >, <=, >=, ==, !=, ===
+
+// const score = 200
+
+// if (scope > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+
+// if (balance>100) console.log("test");
+
+// if (balance>100) console.log("test"), console.log("test2");;
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if(balance<750){
+//     console.log("less than 750");
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2 == 3) {
+//     console.log("Allow to buy course");
+// }
+if(loggedInFromGoogle || loggedInFromEmail || true){
+    console.log("User logged in");
+}
+
