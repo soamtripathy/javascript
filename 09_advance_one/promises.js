3fc60fe5d91b0de644d6fd044c680c528df29d66
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-03-16 16:04:57 +0530

    promises and fetch

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -22,12 +22,75 @@ new Promise(function (resolve, reject) {
   console.log("Async 2 resolved");
 });
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-            resolve({username : "Chai", email : "chai@example.com"})
-    }, 1000)
-})
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "Chai", email: "chai@example.com" });
+  }, 1000);
+});
 
-promiseThree.then(function(user){
-        console.log(user);
-})
+promiseThree.then(function (user) {
+  console.log(user);
+});
+
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "Soam", password: "123" });
+    } else {
+      reject("ERROR : Something went wrong");
+    }
+  }, 1000);
+});
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(() => console.log("The promise is either resolved or rejected."));
+
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "javascript", password: "123" });
+    } else {
+      reject("ERROR : JS went wrong");
+    }
+  }, 1000);
+});
+
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://api.github.com/users/soamtripathy");
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// getAllUsers();
+
+fetch("https://api.github.com/users/soamtripathy")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
